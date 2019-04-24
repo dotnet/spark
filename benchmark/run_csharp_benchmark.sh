f271b655a0c1e8011b18c01baa2e9129c909a810
Steve Suh <stsuh@microsoft.com>
2019-04-24 02:39:21 -0700

    Cold iterations

--- benchmark/run_csharp_benchmark.sh
+++ benchmark/run_csharp_benchmark.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-NUM_EXECUTORS=$1
-DRIVER_MEMORY=$2
-EXECUTOR_MEMORY=$3
-EXECUTOR_CORES=$4
-CSHARP_DLL=$5
-JAR_PATH=$6
-CSHARP_EXECUTABLE=$7
-DATA_PATH=$8
-NUM_ITERATION=$9
-IS_SQL=$10
+COLD_ITERATION=$1
+NUM_EXECUTORS=$2
+DRIVER_MEMORY=$3
+EXECUTOR_MEMORY=$4
+EXECUTOR_CORES=$5
+CSHARP_DLL=$6
+JAR_PATH=$7
+CSHARP_EXECUTABLE=$8
+DATA_PATH=$9
+NUM_ITERATION=$10
+IS_SQL=$11
 
 for i in {1..22}
 do
-  $SPARK_HOME/bin/spark-submit --master yarn --num-executors $NUM_EXECUTORS --driver-memory $DRIVER_MEMORY --executor-memory $EXECUTOR_MEMORY --executor-cores $EXECUTOR_CORES --files $CSHARP_DLL --class org.apache.spark.deploy.DotnetRunner $JAR_PATH $CSHARP_EXECUTABLE $DATA_PATH $i $NUM_ITERATION $IS_SQL
+  for j in $(seq 1 $COLD_ITERATION)
+  do
+    $SPARK_HOME/bin/spark-submit --master yarn --num-executors $NUM_EXECUTORS --driver-memory $DRIVER_MEMORY --executor-memory $EXECUTOR_MEMORY --executor-cores $EXECUTOR_CORES --files $CSHARP_DLL --class org.apache.spark.deploy.DotnetRunner $JAR_PATH $CSHARP_EXECUTABLE $DATA_PATH $i $NUM_ITERATION $IS_SQL
+  done
 done
--- benchmark/run_scala_benchmark.sh
+++ benchmark/run_scala_benchmark.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-NUM_EXECUTORS=$1
-DRIVER_MEMORY=$2
-EXECUTOR_MEMORY=$3
-EXECUTOR_CORES=$4
-JAR_PATH=$5
-DATA_PATH=$6
-NUM_ITERATION=$7
-IS_SQL=$8
+COLD_ITERATION=$1
+NUM_EXECUTORS=$2
+DRIVER_MEMORY=$3
+EXECUTOR_MEMORY=$4
+EXECUTOR_CORES=$5
+JAR_PATH=$6
+DATA_PATH=$7
+NUM_ITERATION=$8
+IS_SQL=$9
 
 for i in {1..22}
 do
-  $SPARK_HOME/bin/spark-submit --master yarn --num-executors $NUM_EXECUTORS --driver-memory $DRIVER_MEMORY --executor-memory $EXECUTOR_MEMORY --executor-cores $EXECUTOR_CORES --class com.microsoft.tpch.App $JAR_PATH $DATA_PATH $i $NUM_ITERATION $IS_SQL
+  for j in $(seq 1 $COLD_ITERATION)
+  do
+    $SPARK_HOME/bin/spark-submit --master yarn --num-executors $NUM_EXECUTORS --driver-memory $DRIVER_MEMORY --executor-memory $EXECUTOR_MEMORY --executor-cores $EXECUTOR_CORES --class com.microsoft.tpch.App $JAR_PATH $DATA_PATH $i $NUM_ITERATION $IS_SQL
+  done
 done
