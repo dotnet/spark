0dc723f8ebc7ee20e4f0adba55042f9bfadb6788
Rahul Potharaju <rapoth@microsoft.com>
2019-05-05 21:17:52 -0700

    Update db-init.sh

--- deployment/db-init.sh
+++ deployment/db-init.sh
@@ -11,17 +11,17 @@
 ##############################################################################
 ################################# CHANGE THESE ###############################
 
-# SPARKDOTNET_RELEASE to point to the appropriate version you downloaded from the
+# DOTNET_SPARK_RELEASE to point to the appropriate version you downloaded from the
 # https://github.com/dotnet/spark Releases section. For instance, for v0.2.0, you
 # would set it to the following URI:
 # https://github.com/dotnet/spark/releases/download/v0.2.0/Microsoft.Spark.Worker.netcoreapp2.1.linux-x64-0.2.0.tar.gz
-SPARKDOTNET_RELEASE=https://github.com/dotnet/spark/releases/download/v0.2.0/Microsoft.Spark.Worker.netcoreapp2.1.linux-x64-0.2.0.tar.gz
+DOTNET_SPARK_RELEASE=https://github.com/dotnet/spark/releases/download/v0.2.0/Microsoft.Spark.Worker.netcoreapp2.1.linux-x64-0.2.0.tar.gz
 
 # No need to change this unless you choose to use a different location
 DBFS_INSTALLATION_ROOT=/dbfs/spark-dotnet
-SPARKDOTNET_WORKER_INSTALLATION_PATH=/usr/local/bin
+DOTNET_SPARK_WORKER_INSTALLATION_PATH=/usr/local/bin
 
 ###############################################################################
 
 set +e
-/bin/bash $DBFS_INSTALLATION_ROOT/$SPARKDOTNET_RELEASE github $DBFS_INSTALLATION_ROOT/$SPARKDOTNET_RELEASE $SPARKDOTNET_WORKER_INSTALLATION_PATH
+/bin/bash $DBFS_INSTALLATION_ROOT/$DOTNET_SPARK_RELEASE github $DBFS_INSTALLATION_ROOT/$DOTNET_SPARK_RELEASE $DOTNET_SPARK_WORKER_INSTALLATION_PATH
