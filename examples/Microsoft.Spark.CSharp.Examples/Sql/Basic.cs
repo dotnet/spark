d1fa16835ec6ef97cd03a7f2e1d7e126e572b787
Terry Kim <yuminkim@gmail.com>
2019-05-20 19:36:11 -0700

    Update example

--- examples/Microsoft.Spark.CSharp.Examples/Sql/Basic.cs
+++ examples/Microsoft.Spark.CSharp.Examples/Sql/Basic.cs
@@ -36,7 +36,7 @@ namespace Microsoft.Spark.Examples.Sql
             Spark.Sql.Types.StructType schema = df.Schema();
             Console.WriteLine(schema.SimpleString);
 
-            System.Collections.Generic.IEnumerable<Row> rows = df.Collect();
+            IEnumerable<Row> rows = df.Collect();
             foreach (Row row in rows)
             {
                 Console.WriteLine(row);
@@ -81,6 +81,17 @@ namespace Microsoft.Spark.Examples.Sql
             // Multiple UDF example:
             df.Select(addition(df["age"], df["name"]), addition2(df["name"])).Show();
 
+            // UDF return type as array.
+            Func<Column, Column> udfArray =
+                Udf<string, string[]>((str) => new string[] { str, str + str });
+            df.Select(Explode(udfArray(df["name"]))).Show();
+
+            // UDF return type as map.
+            Func<Column, Column> udfMap =
+                Udf<string, IDictionary<string, string[]>>(
+                    (str) => new Dictionary<string, string[]> { { str, new[] { str, str } } });
+            df.Select(udfMap(df["name"]).As("UdfMap")).Show(truncate: 50);
+
             // Joins.
             DataFrame joinedDf = df.Join(df, "name");
             joinedDf.Show();
@@ -90,12 +101,6 @@ namespace Microsoft.Spark.Examples.Sql
 
             DataFrame joinedDf3 = df.Join(df, df["name"] == df["name"], "outer");
             joinedDf3.Show();
-
-            var udf = Udf<string, string[]>((str) => new string[] { str, str + str });
-            df.Select(Explode(udf(df["name"]))).Show();
-
-            var udfMap = Udf<string, Dictionary<string, string>>((str) => new Dictionary<string, string> { { str, str + str } });
-            df.Select(udfMap(df["name"])).Show();
 
             spark.Stop();
         }
