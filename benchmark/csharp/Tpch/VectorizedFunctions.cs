dd2cf2553da82234e5f3c894fcae14ee2942afa6
Eric Erhardt <eric.erhardt@microsoft.com>
2019-06-03 12:53:22 -0500

    Unvectorize the vectorized functions.

--- benchmark/csharp/Tpch/VectorizedFunctions.cs
+++ benchmark/csharp/Tpch/VectorizedFunctions.cs
@@ -18,46 +18,53 @@ namespace Tpch
 
             int length = price.Length;
             var builder = new ArrowBuffer.Builder<double>(length);
-            Vector<double> ones = new Vector<double>(1d);
-
-            fixed (double* pStartExtendedPrice = price.Values)
-            fixed (double* pStartDiscount = discount.Values)
-            fixed (double* pStartTax = tax.Values)
+            ReadOnlySpan<double> prices = price.Values;
+            ReadOnlySpan<double> discounts = discount.Values;
+            ReadOnlySpan<double> taxes = tax.Values;
+            for (int i = 0; i < length; i++)
             {
-                double* pExtendedPrice = pStartExtendedPrice;
-                double* pDiscount = pStartDiscount;
-                double* pTax = pStartTax;
-                double* pExtendedPriceEnd = pExtendedPrice + length;
-
-                double* pVectorizationEnd = pExtendedPriceEnd - 4;
-
-                while (pExtendedPrice <= pVectorizationEnd)
-                {
-                    Vector<double> exP = Unsafe.ReadUnaligned<Vector<double>>(pExtendedPrice);
-                    Vector<double> dis = Unsafe.ReadUnaligned<Vector<double>>(pDiscount);
-                    Vector<double> t = Unsafe.ReadUnaligned<Vector<double>>(pTax);
-
-                    Vector<double> r = exP * ((ones - dis) * (ones + t));
-
-                    for (int i = 0; i < Vector<double>.Count; i++)
-                    {
-                        builder.Append(r[i]);
-                    }
-
-                    pExtendedPrice += 4;
-                    pDiscount += 4;
-                    pTax += 4;
-                }
-
-                while (pExtendedPrice <= pExtendedPriceEnd)
-                {
-                    builder.Append(*pExtendedPrice * (1 - *pDiscount) * (1 + *pTax));
-
-                    pExtendedPrice++;
-                    pDiscount++;
-                    pTax++;
-                }
+                builder.Append(prices[i] * (1 - discounts[i]) * (1 + taxes[i]));
             }
+            //Vector<double> ones = new Vector<double>(1d);
+
+            //fixed (double* pStartExtendedPrice = price.Values)
+            //fixed (double* pStartDiscount = discount.Values)
+            //fixed (double* pStartTax = tax.Values)
+            //{
+            //    double* pExtendedPrice = pStartExtendedPrice;
+            //    double* pDiscount = pStartDiscount;
+            //    double* pTax = pStartTax;
+            //    double* pExtendedPriceEnd = pExtendedPrice + length;
+
+            //    double* pVectorizationEnd = pExtendedPriceEnd - 4;
+
+            //    while (pExtendedPrice <= pVectorizationEnd)
+            //    {
+            //        Vector<double> exP = Unsafe.ReadUnaligned<Vector<double>>(pExtendedPrice);
+            //        Vector<double> dis = Unsafe.ReadUnaligned<Vector<double>>(pDiscount);
+            //        Vector<double> t = Unsafe.ReadUnaligned<Vector<double>>(pTax);
+
+            //        Vector<double> r = exP * ((ones - dis) * (ones + t));
+
+            //        for (int i = 0; i < Vector<double>.Count; i++)
+            //        {
+            //            builder.Append(r[i]);
+            //        }
+
+            //        pExtendedPrice += 4;
+            //        pDiscount += 4;
+            //        pTax += 4;
+            //    }
+
+            //    while (pExtendedPrice <= pExtendedPriceEnd)
+            //    {
+            //        builder.Append(*pExtendedPrice * (1 - *pDiscount) * (1 + *pTax));
+
+            //        pExtendedPrice++;
+            //        pDiscount++;
+            //        pTax++;
+            //    }
+            //}
 
             return new DoubleArray(
                 builder.Build(),
@@ -74,41 +81,48 @@ namespace Tpch
 
             int length = price.Length;
             var builder = new ArrowBuffer.Builder<double>(length);
-            Vector<double> ones = new Vector<double>(1d);
-
-            fixed (double* pStartExtendedPrice = price.Values)
-            fixed (double* pStartDiscount = discount.Values)
+            ReadOnlySpan<double> prices = price.Values;
+            ReadOnlySpan<double> discounts = discount.Values;
+            for (int i = 0; i < length; i++)
             {
-                double* pExtendedPrice = pStartExtendedPrice;
-                double* pDiscount = pStartDiscount;
-                double* pExtendedPriceEnd = pExtendedPrice + length;
-
-                double* pVectorizationEnd = pExtendedPriceEnd - 4;
-
-                while (pExtendedPrice <= pVectorizationEnd)
-                {
-                    Vector<double> exP = Unsafe.ReadUnaligned<Vector<double>>(pExtendedPrice);
-                    Vector<double> dis = Unsafe.ReadUnaligned<Vector<double>>(pDiscount);
-
-                    Vector<double> r = exP * (ones - dis);
-
-                    for (int i = 0; i < Vector<double>.Count; i++)
-                    {
-                        builder.Append(r[i]);
-                    }
-
-                    pExtendedPrice += 4;
-                    pDiscount += 4;
-                }
-
-                while (pExtendedPrice <= pExtendedPriceEnd)
-                {
-                    builder.Append(*pExtendedPrice * (1 - *pDiscount));
-
-                    pExtendedPrice++;
-                    pDiscount++;
-                }
+                builder.Append(prices[i] * (1 - discounts[i]));
             }
+
+            //Vector<double> ones = new Vector<double>(1d);
+
+            //fixed (double* pStartExtendedPrice = price.Values)
+            //fixed (double* pStartDiscount = discount.Values)
+            //{
+            //    double* pExtendedPrice = pStartExtendedPrice;
+            //    double* pDiscount = pStartDiscount;
+            //    double* pExtendedPriceEnd = pExtendedPrice + length;
+
+            //    double* pVectorizationEnd = pExtendedPriceEnd - 4;
+
+            //    while (pExtendedPrice <= pVectorizationEnd)
+            //    {
+            //        Vector<double> exP = Unsafe.ReadUnaligned<Vector<double>>(pExtendedPrice);
+            //        Vector<double> dis = Unsafe.ReadUnaligned<Vector<double>>(pDiscount);
+
+            //        Vector<double> r = exP * (ones - dis);
+
+            //        for (int i = 0; i < Vector<double>.Count; i++)
+            //        {
+            //            builder.Append(r[i]);
+            //        }
+
+            //        pExtendedPrice += 4;
+            //        pDiscount += 4;
+            //    }
+
+            //    while (pExtendedPrice <= pExtendedPriceEnd)
+            //    {
+            //        builder.Append(*pExtendedPrice * (1 - *pDiscount));
+
+            //        pExtendedPrice++;
+            //        pDiscount++;
+            //    }
+            //}
 
             return new DoubleArray(
                 builder.Build(),
