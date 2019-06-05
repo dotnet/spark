fb8d8d9a1c588a14211dd5d764cad22269c94c32
Eric Erhardt <eric.erhardt@microsoft.com>
2019-06-05 18:06:02 -0500

    Initial WIP

---
+++ src/csharp/Microsoft.Spark.Experimental/Sql/RelationalGroupedDatasetExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Apache.Arrow;
+using Microsoft.Spark.Sql.Types;
+
+namespace Microsoft.Spark.Sql
+{
+    /// <summary>
+    /// Extension methods for <see cref="RelationalGroupedDataset"/>.
+    /// </summary>
+    public static class RelationalGroupedDatasetExtensions
+    {
+        public static DataFrame Apply(
+            this RelationalGroupedDataset dataset,
+            string name, 
+            StructType returnType, 
+            Func<RecordBatch, RecordBatch> func)
+        {
+            return dataset.Apply(name, returnType, func);
+        }
+    }
+}
--- src/csharp/Microsoft.Spark/Sql/RelationalGroupedDataset.cs
+++ src/csharp/Microsoft.Spark/Sql/RelationalGroupedDataset.cs
@@ -2,7 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using Apache.Arrow;
 using Microsoft.Spark.Interop.Ipc;
+using Microsoft.Spark.Sql.Expressions;
+using Microsoft.Spark.Sql.Types;
+using Microsoft.Spark.Utils;
 
 namespace Microsoft.Spark.Sql
 {
@@ -38,6 +43,38 @@ namespace Microsoft.Spark.Sql
         public DataFrame Count()
         {
             return new DataFrame((JvmObjectReference)_jvmObject.Invoke("count"));
+        }
+
+        internal DataFrame Apply(string name, StructType returnType, Func<RecordBatch, RecordBatch> func)
+        {
+            // TODO: func needs to change to be our *UdfWrapper
+            byte[] command = CommandSerDe.Serialize(
+                func,
+                CommandSerDe.SerializedMode.Row,
+                CommandSerDe.SerializedMode.Row);
+
+            JvmObjectReference pythonFunction =
+                UdfUtils.CreatePythonFunction(_jvmObject.Jvm, command);
+
+            var udf = new UserDefinedFunction(
+                _jvmObject.Jvm.CallConstructor(
+                    "org.apache.spark.sql.execution.python.UserDefinedPythonFunction",
+                    name,
+                    pythonFunction,
+                    GetDataType(returnType),
+                    (int)UdfUtils.PythonEvalType.SQL_GROUPED_MAP_PANDAS_UDF,
+                    true // udfDeterministic
+                    ));
+
+            return new DataFrame((JvmObjectReference)_jvmObject.Invoke("flatMapGroupsInPandas", udf));
+        }
+
+        private JvmObjectReference GetDataType(StructType type)
+        {
+            return (JvmObjectReference)_jvmObject.Jvm.CallStaticJavaMethod(
+                "org.apache.spark.sql.types.DataType",
+                "fromJson",
+                type.Json);
         }
     }
 }
