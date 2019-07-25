e967466dd45b5287b455e023f81fe6073e0fe6e0
Steve Suh <suhsteve@gmail.com>
2019-07-25 02:29:28 -0700

    add udfserde E2E tests

---
+++ external/Microsoft.Spark.E2ETest.ExternalLibrary/ExternalClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Microsoft.Spark.E2ETest.ExternalLibrary
+{
+    [Serializable]
+    public class ExternalClass
+    {
+        private string _s;
+
+        public ExternalClass(string s)
+        {
+            _s = s;
+        }
+
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public string Concat(string s)
+        {
+            return _s + s;
+        }
+    }
+}
---
+++ src/csharp/Microsoft.Spark.E2ETest/UdfTests/UdfSerDeTests.cs
@@ -0,0 +1,67 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.Spark.E2ETest.ExternalLibrary;
+using Microsoft.Spark.Sql;
+using Xunit;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace Microsoft.Spark.E2ETest.UdfTests
+{
+    [Collection("Spark E2E Tests")]
+    public class UdfSerDeTests
+    {
+        private readonly SparkSession _spark;
+        private readonly DataFrame _df;
+
+        public UdfSerDeTests(SparkFixture fixture)
+        {
+            _spark = fixture.Spark;
+            _df = _spark
+                .Read()
+                .Schema("age INT, name STRING")
+                .Json($"{TestEnvironment.ResourceDirectory}people.json");
+        }
+
+        [Fact]
+        public void TestUdfClosure()
+        {
+            var ec = new ExternalClass("Hello");
+            Func<Column, Column> udf = Udf<string, string>(
+                (str) =>
+                {
+                    return ec.Concat(str);
+                });
+
+            _df.Select(udf(_df["name"])).Show();
+        }
+
+        [Fact]
+        public void TestExternalStaticMethodCall()
+        {
+            Func<Column, Column> udf = Udf<string, string>(str =>
+            {
+                ExternalClass.HelloWorld();
+                return str;
+            });
+
+            _df.Select(udf(_df["name"])).Show();
+        }
+
+        [Fact]
+        public void TestInitExternalClassInUdf()
+        {
+            // Instantiate external assembly class within body of Udf.
+            Func<Column, Column> udf = Udf<string, string>(
+                    (str) =>
+                    {
+                        var ec = new ExternalClass("Hello");
+                        return ec.Concat(str);
+                    });
+
+            _df.Select(udf(_df["name"])).Show();
+        }
+    }
+}
