d09a67e8142ab7c699c26a3be1ddb0fcebfd41d6
Ed Elliott <goeddieuk@gmail.com>
2019-08-30 17:21:29 +0100

    bare bones for Table

--- src/csharp/Microsoft.Spark/Sql/Table.cs
+++ src/csharp/Microsoft.Spark/Sql/Table.cs
@@ -12,5 +12,41 @@ namespace Microsoft.Spark.Sql
         }
 
         JvmObjectReference IJvmObjectReferenceProvider.Reference => _jvmObject;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>string</returns>
+        public string Database => (string)_jvmObject.Invoke("database");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>string</returns>
+        public string Description => (string)_jvmObject.Invoke("description");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>string</returns>
+        public bool IsTemporary => (bool)_jvmObject.Invoke("isTemporary");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>string</returns>
+        public string Name => (string)_jvmObject.Invoke("name");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>string</returns>
+        public string TableType => (string)_jvmObject.Invoke("tableType");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>string</returns>
+        public new string ToString => (string)_jvmObject.Invoke("toString");
     }
 }
