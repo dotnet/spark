b8edbbf1e452dea74c087ce7c97b379e77aedfae
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2019-09-17 11:17:00 -0700

    Support for DeltaTable (#236)

---
+++ src/csharp/Extensions/Microsoft.Spark.Extensions.Delta/Tables/DeltaMergeMatchedActionBuilder.cs
@@ -0,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Microsoft.Spark.Interop.Ipc;
+using Microsoft.Spark.Sql;
+
+namespace Microsoft.Spark.Extensions.Delta.Tables
+{
+    /// <summary>
+    /// Builder class to specify the actions to perform when a target table row has matched a
+    /// source row based on the given merge condition and optional match condition.
+    /// </summary>
+    public class DeltaMergeMatchedActionBuilder : IJvmObjectReferenceProvider
+    {
+        private readonly JvmObjectReference _jvmObject;
+
+        internal DeltaMergeMatchedActionBuilder(JvmObjectReference jvmObject)
+        {
+            _jvmObject = jvmObject;
+        }
+
+        JvmObjectReference IJvmObjectReferenceProvider.Reference => _jvmObject;
+
+        /// <summary>
+        /// Update the matched table rows based on the rules defined by <c>set</c>.
+        /// </summary>
+        /// <param name="set">Rules to update a row as amap between target column names and
+        /// corresponding update expressions as Column objects.</param>
+        /// <returns>DeltaMergeBuilder object.</returns>
+        public DeltaMergeBuilder Update(Dictionary<string, Column> set) =>
+            new DeltaMergeBuilder((JvmObjectReference)_jvmObject.Invoke("update", set));
+
+        /// <summary>
+        /// Update the matched table rows based on the rules defined by <c>set</c>.
+        /// </summary>
+        /// <param name="set">Rules to update a row as a map between target column names and
+        /// corresponding update expressions as SQL formatted strings.</param>
+        /// <returns>DeltaMergeBuilder object.</returns>
+        public DeltaMergeBuilder UpdateExpr(Dictionary<string, string> set) =>
+            new DeltaMergeBuilder((JvmObjectReference)_jvmObject.Invoke("updateExpr", set));
+
+        /// <summary>
+        /// Update all the columns of the matched table row with the values of the corresponding
+        /// columns in the source row.
+        /// </summary>
+        /// <returns>DeltaMergeBuilder object.</returns>
+        public DeltaMergeBuilder UpdateAll() =>
+            new DeltaMergeBuilder((JvmObjectReference)_jvmObject.Invoke("updateAll"));
+
+        /// <summary>
+        /// Delete a matched row from the table.
+        /// </summary>
+        /// <returns>DeltaMergeBuilder object.</returns>
+        public DeltaMergeBuilder Delete() =>
+            new DeltaMergeBuilder((JvmObjectReference)_jvmObject.Invoke("delete"));
+    }
+}
