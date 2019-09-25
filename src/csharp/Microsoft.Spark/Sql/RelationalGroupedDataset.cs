d7486611846e590a3b52f5c12c7c2cdb64598308
Steve Suh <suhsteve@gmail.com>
2019-09-25 02:03:34 -0700

    initial commit

--- src/csharp/Microsoft.Spark/Sql/RelationalGroupedDataset.cs
+++ src/csharp/Microsoft.Spark/Sql/RelationalGroupedDataset.cs
@@ -34,19 +34,22 @@ namespace Microsoft.Spark.Sql
         /// <param name="expr">Column to aggregate on</param>
         /// <param name="exprs">Additional columns to aggregate on</param>
         /// <returns>New DataFrame object with aggregation applied</returns>
-        public DataFrame Agg(Column expr, params Column[] exprs)
-        {
-            return new DataFrame((JvmObjectReference)_jvmObject.Invoke("agg", expr, exprs));
-        }
+        public DataFrame Agg(Column expr, params Column[] exprs) =>
+            new DataFrame((JvmObjectReference)_jvmObject.Invoke("agg", expr, exprs));
 
         /// <summary>
         /// Count the number of rows for each group.
         /// </summary>
         /// <returns>New DataFrame object with count applied</returns>
-        public DataFrame Count()
-        {
-            return new DataFrame((JvmObjectReference)_jvmObject.Invoke("count"));
-        }
+        public DataFrame Count() =>
+            new DataFrame((JvmObjectReference)_jvmObject.Invoke("count"));
+
+        /// <summary>
+        /// Compute the sum for each numeric columns for each group.
+        /// </summary>
+        /// <returns>New DataFrame object with sum applied</returns>
+        public DataFrame Sum(params string[] colNames) =>
+            new DataFrame((JvmObjectReference)_jvmObject.Invoke("sum", (object)colNames));
 
         internal DataFrame Apply(StructType returnType, Func<RecordBatch, RecordBatch> func)
         {
