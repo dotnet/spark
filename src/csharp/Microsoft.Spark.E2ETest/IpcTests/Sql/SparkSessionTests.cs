3292df0da5d8f57873b7cf5eca6a2b9fdaba46a8
Terry Kim <terryk@microsoft.com>
2019-09-25 12:19:02 -0700

    add missing file

---
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/Sql/SparkSessionTests.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Spark.E2ETest.Utils;
+using Microsoft.Spark.Sql;
+using Xunit;
+
+namespace Microsoft.Spark.E2ETest.IpcTests
+{
+    [Collection("Spark E2E Tests")]
+    public class SparkSessionTests
+    {
+        private readonly SparkSession _spark;
+
+        public SparkSessionTests(SparkFixture fixture)
+        {
+            _spark = fixture.Spark;
+        }
+
+        /// <summary>
+        /// Test signatures for APIs up to Spark 2.3.*.
+        /// The purpose of this test is to ensure that JVM calls can be successfully made.
+        /// Note that this is not testing functionality of each function.
+        /// </summary>
+        [Fact]
+        public void TestSignaturesV2_3_X()
+        {
+            SparkSession.SetActiveSession(_spark);
+
+            SparkSession.SetDefaultSession(_spark);
+
+            Assert.IsType<SparkSession>(SparkSession.GetActiveSession());
+
+            Assert.IsType<SparkSession>(SparkSession.GetDefaultSession());
+        }
+
+        /// <summary>
+        /// Test signatures for APIs introduced in Spark 2.4.*.
+        /// </summary>
+        [SkipIfSparkVersionIsLessThan(Versions.V2_4_0)]
+        public void TestSignaturesV2_4_X()
+        {
+            Assert.IsType<SparkSession>(SparkSession.Active());
+        }
+    }
+}
