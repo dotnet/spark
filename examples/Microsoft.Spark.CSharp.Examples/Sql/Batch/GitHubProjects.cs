f5b2119fab5c8c8f08e11b8a3002d6de4569eab1
Brigit Murtaugh <bamurtaugh@yahoo.com>
2019-10-10 12:54:17 -0700

    Add GitHubProjects
    App used in demos (dotnetconf, 101 videos)

---
+++ examples/Microsoft.Spark.CSharp.Examples/Sql/Batch/GitHubProjects.cs
@@ -0,0 +1,79 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace Microsoft.Spark.Examples.Sql.Batch
+{
+    /// <summary>
+    /// An example analyzing GitHub projects
+    /// data, demonstrating Spark SQL features.
+    /// </summary>
+    internal sealed class GitHubProjects : IExample
+    {
+        public void Run(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine( 
+                    "Usage: GitHubProjects <path to projects.csv>");
+                Environment.Exit(1);
+            }
+
+            SparkSession spark = SparkSession
+                .Builder()
+                .AppName("GitHub and Spark Batch")
+                .GetOrCreate();
+
+            DataFrame projectsDf = spark
+              .Read()
+              .Schema("id INT, url STRING, owner_id INT, name STRING, descriptor STRING, language STRING, created_at STRING, forked_from INT, deleted STRING, updated_at STRING")
+              .Csv(args[0]);
+
+            projectsDf.Show();
+
+            // Drop any rows with NA values
+            DataFrameNaFunctions dropEmptyProjects = projectsDf.Na();
+            DataFrame cleanedProjects = dropEmptyProjects.Drop("any");
+
+            // Remove unnecessary columns
+            cleanedProjects = cleanedProjects.Drop("id", "url", "owner_id");
+            cleanedProjects.Show();
+
+            // Get the average number of times each language has been forked
+            DataFrame groupedDF = cleanedProjects.GroupBy("language").Agg(Avg(cleanedProjects["forked_from"]));
+
+            // Sort by most forked languages first
+            groupedDF.OrderBy(Desc("avg(forked_from)")).Show();
+
+            // Find projects updated since 10/20/15
+            spark.Udf().Register<string, bool>("MyUDF", (date) => DateTest(date));
+            cleanedProjects.CreateOrReplaceTempView("dateView");
+            DataFrame dateDf = spark.Sql("SELECT *, MyUDF(dateView.updated_at) AS datebefore FROM dateView");
+
+            spark.Stop();
+        }
+
+        public static bool DateTest(string date)
+        {
+            //  Remove invalid dates to avoid: System.FormatException: String '0000-00-00 00:00:00' was not recognized as a valid DateTime
+            if (date.Equals("0000-00-00 00:00:00"))
+                return false;
+
+            DateTime convertedDate = Convert.ToDateTime(date);
+
+            // 10/20/2015 
+            DateTime referenceDate = new DateTime(2015, 10, 20);
+
+            // > 0 means convertedDate (input from file) is later than 10/20/15
+            if (DateTime.Compare(convertedDate, referenceDate) > 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
