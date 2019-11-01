052c5368c9619a90fa75db27d8f9fd6120561ab3
Brigit Murtaugh <bamurtaugh@yahoo.com>
2019-11-01 16:01:31 -0700

    Add separate project
    ML.NET working in separate project, and other examples still build/compile successfully

---
+++ examples/Microsoft.Spark.CSharp.Examples/Sentiment/MLSparkVideoML.Model/DataModels/ModelInput.cs
@@ -0,0 +1,22 @@
+//*****************************************************************************************
+//*                                                                                       *
+//* This is an auto-generated file by Microsoft ML.NET CLI (Command-Line Interface) tool. *
+//*                                                                                       *
+//*****************************************************************************************
+
+using Microsoft.ML.Data;
+
+namespace MLSparkVideoML.Model.DataModels
+{
+    public class ModelInput
+    {
+        [ColumnName("Column1"), LoadColumn(0)]
+        public string Column1 { get; set; }
+
+
+        [ColumnName("Column2"), LoadColumn(1)]
+        public bool Column2 { get; set; }
+
+
+    }
+}
