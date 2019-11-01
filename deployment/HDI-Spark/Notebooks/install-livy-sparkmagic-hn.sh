c93966252beac5b2be37c2c6fe0b5e79b4ba6524
elvaliuliuliu <47404285+elvaliuliuliu@users.noreply.github.com>
2019-11-01 15:45:43 -0700

    Resolve comments

--- deployment/HDI-Spark/Notebooks/install-livy-sparkmagic-hn.sh
+++ deployment/HDI-Spark/Notebooks/install-livy-sparkmagic-hn.sh
@@ -3,28 +3,20 @@
 # Update Livy Jars
 #
 
-sudo wget https://ruinliureplhdistorage.blob.core.windows.net/ruinliurepl/livy_jar.zip -O livy_jar.zip
+sudo wget https://ruinliureplhdistorage.blob.core.windows.net/ruinliurepl/livy_jar.zip
 sudo unzip livy_jar.zip
 
-if [ "$(ls -A /usr/hdp/current/livy2-server/jars)" ]; then
-     sudo rm /usr/hdp/current/livy2-server/jars/*
-fi
-if [ "$(ls -A /usr/hdp/current/livy2-server/repl_2.11-jars)" ]; then
-     sudo rm /usr/hdp/current/livy2-server/repl_2.11-jars/*
-fi
-if [ "$(ls -A /usr/hdp/current/livy2-server/rsc-jars)" ]; then
-     sudo rm /usr/hdp/current/livy2-server/rsc-jars/*
-fi
+rm -f /usr/hdp/current/livy2-server/jars/*
+rm -f /usr/hdp/current/livy2-server/repl_2.11-jars/*
+rm -f /usr/hdp/current/livy2-server/rsc-jars/*
 
-sudo cp livy_jar/jars/* /usr/hdp/current/livy2-server/jars/
-sudo cp livy_jar/repl_2.11-jars/* /usr/hdp/current/livy2-server/repl_2.11-jars/
-sudo cp livy_jar/rsc-jars/* /usr/hdp/current/livy2-server/rsc-jars/
+sudo cp livy_jar/* /usr/hdp/current/livy2-server/
 
 #
 # Update SparkMagic
 #
 
-sudo wget https://ruinliureplhdistorage.blob.core.windows.net/ruinliurepl/sparkmagic.zip -O sparkmagic.zip
+sudo wget https://ruinliureplhdistorage.blob.core.windows.net/ruinliurepl/sparkmagic.zip
 sudo unzip sparkmagic.zip
 
 sudo cp sparkmagic/config.json /home/spark/.sparkmagic/
@@ -38,17 +30,15 @@ if [ ! -d "/usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/kernels/spar
 then
     sudo mkdir /usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/kernels/sparkdotnetkernel
 fi
-sudo cp sparkmagic/kernels/sparkdotnetkernel/* /usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/kernels/sparkdotnetkernel/
+sudo cp sparkmagic/* /usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/
+
 # install sparkdotnetkernel
 sudo /usr/bin/anaconda/bin/jupyter-kernelspec install /usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/kernels/sparkdotnetkernel
 
-sudo cp sparkmagic/magics/remotesparkmagics.py /usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/magics/remotesparkmagics.py
-sudo cp sparkmagic/magics/sparkmagicsbase.py /usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/magics/sparkmagicsbase.py
-
-sudo cp sparkmagic/utils/configuration.py /usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/utils/configuration.py
-sudo cp sparkmagic/utils/constants.py /usr/bin/anaconda/lib/python2.7/site-packages/sparkmagic/utils/constants.py
-
+#
 # Remove the temporary livy jars and sparkmagic files.
+#
+
 sudo rm livy_jar.zip
 sudo rm -rf livy_jar
 sudo rm sparkmagic.zip
