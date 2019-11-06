f79ab60076eab17de0cb491521f74c13d67ca50c
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2019-11-06 11:06:41 -0800

    DataStreamReader tests

--- src/csharp/Microsoft.Spark.E2ETest/IpcTests/Sql/Streaming/DataStreamReaderTests.cs
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/Sql/Streaming/DataStreamReaderTests.cs
@@ -2,12 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using Microsoft.Spark.E2ETest.Utils;
 using Microsoft.Spark.Sql;
 using Microsoft.Spark.Sql.Streaming;
 using Xunit;
 
-namespace Microsoft.Spark.E2ETest.IpcTests.Sql.Streaming
+namespace Microsoft.Spark.E2ETest.IpcTests
 {
     [Collection("Spark E2E Tests")]
     public class DataStreamReaderTests
@@ -25,29 +28,39 @@ namespace Microsoft.Spark.E2ETest.IpcTests.Sql.Streaming
         [Fact]
         public void TestSignaturesV2_3_X()
         {
-            DataFrame df = _spark
-                .ReadStream()
-                .Format("rate")
-                .Option("rowsPerSecond", 1)
-                .Load();
-
             DataStreamReader dsr = _spark.ReadStream();
 
-            Assert.IsType<DataStreamReader>(dsr.Format("source"));
-            Assert.IsType<DataStreamReader>(dsr.Schema("schematring"));
+            Assert.IsType<DataStreamReader>(dsr.Format("parquet"));
+            Assert.IsType<DataStreamReader>(dsr.Schema("columnName bigint"));
             Assert.IsType<DataStreamReader>(dsr.Option("key", "value"));
             Assert.IsType<DataStreamReader>(dsr.Option("key", false));
             Assert.IsType<DataStreamReader>(dsr.Option("key", long.MaxValue));
             Assert.IsType<DataStreamReader>(dsr.Option("key", double.MaxValue));
             Assert.IsType<DataStreamReader>(dsr.Options(new Dictionary<string, string>()));
 
-            Assert.IsType<DataFrame>(dsr.Load());
-            Assert.IsType<DataFrame>(dsr.Load("path"));
-            Assert.IsType<DataFrame>(dsr.Json("path"));
-            Assert.IsType<DataFrame>(dsr.Csv("path"));
-            Assert.IsType<DataFrame>(dsr.Orc("path"));
-            Assert.IsType<DataFrame>(dsr.Parquet("path"));
-            Assert.IsType<DataFrame>(dsr.Text("path"));
+            DataFrame data = _spark.Range(0, 5);
+            using (var tempDirectory = new TemporaryDirectory())
+            {
+                string path = Path.Combine(tempDirectory.Path, Guid.NewGuid().ToString());
+
+                data.Write().Json(path);
+                Assert.IsType<DataFrame>(dsr.Format("json").Option("path", path).Load());
+                Assert.IsType<DataFrame>(dsr.Format("json").Load(path));
+                Assert.IsType<DataFrame>(dsr.Json(path));
+
+                data.Write().Mode("overwrite").Csv(path);
+                Assert.IsType<DataFrame>(dsr.Csv(path));
+
+                data.Write().Mode("overwrite").Orc(path);
+                Assert.IsType<DataFrame>(dsr.Orc(path));
+
+                data.Write().Mode("overwrite").Parquet(path);
+                Assert.IsType<DataFrame>(dsr.Parquet(path));
+            }
+
+            // Text is a special case, because we can't use Range() data.
+            string textFilePath = Path.GetFullPath("Resources/people.txt");
+            Assert.IsType<DataFrame>(dsr.Text(textFilePath));
         }
     }
 }
