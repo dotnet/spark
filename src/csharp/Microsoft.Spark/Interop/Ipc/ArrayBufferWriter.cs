1226fc6bd5c01e3723f7310598a61428c4f75fa2
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2019-12-13 13:32:26 +1100

    Address feedback regarding ArrayBufferWriter source and build warning

--- src/csharp/Microsoft.Spark/Interop/Ipc/ArrayBufferWriter.cs
+++ src/csharp/Microsoft.Spark/Interop/Ipc/ArrayBufferWriter.cs
@@ -2,6 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+// Included from https://github.com/dotnet/runtime/blob/master/src/libraries/Common/src/System/Buffers/ArrayBufferWriter.cs
+// whilst TFM includes netstandard2.0
+
 using System.Diagnostics;
 
 namespace System.Buffers
