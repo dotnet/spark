8e1863ede387a73d0974406c11917ea0368b5aa6
Elva Liu <ruinliu@microsoft.com>
2020-03-19 01:21:36 -0700

    add unit test

---
+++ src/csharp/Microsoft.Spark.UnitTest/Sql/TimestampTests.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.Spark.Sql.Types;
+using Xunit;
+
+namespace Microsoft.Spark.UnitTest
+{
+    public class TimestampTests
+    {
+        [Fact]
+        public void TimestampTest()
+        {
+            var timestamp = new Timestamp(new DateTime(2020, 1, 2, 15, 30, 30, DateTimeKind.Utc));
+
+            // Validate values.
+            Assert.Equal(2020, timestamp.Year);
+            Assert.Equal(1, timestamp.Month);
+            Assert.Equal(2, timestamp.Day);
+            Assert.Equal(15, timestamp.Hour);
+            Assert.Equal(30, timestamp.Minute);
+            Assert.Equal(30, timestamp.Second);
+            Assert.Equal(0, timestamp.Microsecond);
+
+            // Validate ToString().
+            Assert.Equal("2020-01-02 15:30:30.000000", timestamp.ToString());
+
+            // Validate ToDateTime().
+            Assert.IsType<DateTime>(timestamp.ToDateTime());
+
+            // Validate TimeZone when using DateTime to create Timestamp objects.
+            Assert.Throws<InvalidTimeZoneException>(
+                () => new Timestamp(new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Local)));
+        }
+    }
+}
--- src/csharp/Microsoft.Spark/Sql/Types/Timestamp.cs
+++ src/csharp/Microsoft.Spark/Sql/Types/Timestamp.cs
@@ -103,8 +103,7 @@ namespace Microsoft.Spark.Sql.Types
         /// <summary>
         /// Readable string representation for this type.
         /// </summary>
-        public override string ToString() => _dateTime.ToUniversalTime()
-            .ToString("yyyy-MM-dd HH:mm:ss.ffffff");
+        public override string ToString() => _dateTime.ToString("yyyy-MM-dd HH:mm:ss.ffffff");
 
         /// <summary>
         /// Checks if the given object is same as the current object.
