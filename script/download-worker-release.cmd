aa26c7d7ed581762e7662d728f76b0569ad5b237
Niharika Dutta <nidutta@microsoft.com>
2020-03-26 21:33:07 -0700

    Adding testing E2E tests against oldest compatible worker version

---
+++ script/download-worker-release.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+setlocal
+
+set OutputDir=%1
+cd %OutputDir%
+
+echo "Download oldest backwards compatible worker release- 0.9.0 for current version 0.10.0."
+curl -k -L -o Microsoft.Spark.Worker.zip https://github.com/dotnet/spark/releases/download/v0.9.0/Microsoft.Spark.Worker.netcoreapp3.1.linux-x64-0.9.0.zip
+unzip Microsoft.Spark.Worker.zip
+mkdir -p Microsoft.Spark.Worker
+cp -a Microsoft.Spark.Worker-0.9.0/ Microsoft.Spark.Worker/
+
+endlocal
