68e07222e57054c1b90b50de3f577763af2ab144
Niharika Dutta <nidutta@microsoft.com>
2020-03-28 14:40:58 -0700

    testing variable in yml pipeline

--- script/download-worker-release.cmd
+++ script/download-worker-release.cmd
@@ -5,7 +5,7 @@ setlocal
 set OutputDir=%1
 cd %OutputDir%
 
-echo "Download oldest backwards compatible worker release- 0.9.0 for current version 0.10.0."
+echo "Download oldest backwards compatible worker release"
 curl -k -L -o Microsoft.Spark.Worker.zip https://github.com/dotnet/spark/releases/download/v0.9.0/Microsoft.Spark.Worker.netcoreapp3.1.linux-x64-0.9.0.zip
 unzip Microsoft.Spark.Worker.zip
 mkdir -p Microsoft.Spark.Worker
