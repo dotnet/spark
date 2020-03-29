d9d0dfa8f36c2d4b76bfcecaea5e9fcd51a7096b
Niharika Dutta <nidutta@microsoft.com>
2020-03-28 21:20:59 -0700

    fixing directory error

--- script/download-worker-release.cmd
+++ script/download-worker-release.cmd
@@ -8,7 +8,5 @@ cd %OutputDir%
 echo "Download oldest backwards compatible worker release"
 curl -k -L -o Microsoft.Spark.Worker.zip https://github.com/dotnet/spark/releases/download/v0.8.0/Microsoft.Spark.Worker.netcoreapp3.1.linux-x64-0.8.0.zip
 unzip Microsoft.Spark.Worker.zip
-mkdir -p Microsoft.Spark.Worker
-cp -a Microsoft.Spark.Worker-0.8.0/ Microsoft.Spark.Worker/
 
 endlocal
