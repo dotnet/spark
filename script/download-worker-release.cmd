5a2951ac3c76d9c9798c6bfa12bb287c1a0f9aff
Niharika Dutta <nidutta@microsoft.com>
2020-04-08 02:17:16 -0700

    PR review comment changes

--- script/download-worker-release.cmd
+++ script/download-worker-release.cmd
@@ -3,10 +3,11 @@
 setlocal
 
 set OutputDir=%1
+set OldestCompatibleWorkerVersion=%2
 cd %OutputDir%
 
-echo "Download oldest backwards compatible worker release"
-curl -k -L -o Microsoft.Spark.Worker.zip https://github.com/dotnet/spark/releases/download/v0.9.0/Microsoft.Spark.Worker.netcoreapp3.1.win-x64-0.9.0.zip
+echo "Download oldest backwards compatible worker release - %OldestCompatibleWorkerVersion%"
+curl -k -L -o Microsoft.Spark.Worker.zip https://github.com/dotnet/spark/releases/download/v%OldestCompatibleWorkerVersion%/Microsoft.Spark.Worker.netcoreapp3.1.win-x64-%OldestCompatibleWorkerVersion%.zip
 unzip Microsoft.Spark.Worker.zip
 
 endlocal
