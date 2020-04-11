e75996168fb93b0b8b0218d4a941a895116f12c3
Terry Kim <yuminkim@gmail.com>
2020-04-11 10:49:37 -0700

    changes

--- src/csharp/Extensions/Microsoft.Spark.Extensions.Azure.Synapse.Analytics/Utils/TokenLibrary.cs
+++ src/csharp/Extensions/Microsoft.Spark.Extensions.Azure.Synapse.Analytics/Utils/TokenLibrary.cs
@@ -2,9 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Spark.Interop;
+
 namespace Microsoft.Spark.Extensions.Azure.Synapse.Analytics.Utils
 {
     public sealed class TokenLibrary
     {
+        /// <summary>
+        /// Get a connection string for a given linked service.
+        /// </summary>
+        /// <param name="linkedServiceName">Name of the linked service</param>
+        /// <returns>Connection string</returns>
+        public static string GetConnectionString(string linkedServiceName) =>
+            (string)SparkEnvironment.JvmBridge.CallStaticJavaMethod(
+                "com.microsoft.azure.synapse.tokenlibrary.TokenLibrary",
+                "getConnectionString",
+                linkedServiceName);
     }
 }
