86b4348a06b7297c65a9cbeff731cdbf4c743a6c
Steve Suh <suhsteve@gmail.com>
2020-05-28 16:39:02 -0700

    add header, clean usings

--- src/csharp/Microsoft.Spark.UnitTest/CollectionUtilsTests.cs
+++ src/csharp/Microsoft.Spark.UnitTest/CollectionUtilsTests.cs
@@ -1,6 +1,7 @@
-﻿using System.IO;
-using System.Linq;
-using Microsoft.Spark.UnitTest.TestUtils;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using Microsoft.Spark.Utils;
 using Xunit;
 
