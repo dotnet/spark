419ae431a37e36fae471cdbf5b08127a771f0d51
Steve Suh <suhsteve@gmail.com>
2020-05-28 16:36:26 -0700

    Add CollectionUtilsTests

---
+++ src/csharp/Microsoft.Spark.UnitTest/CollectionUtilsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.Spark.UnitTest.TestUtils;
+using Microsoft.Spark.Utils;
+using Xunit;
+
+namespace Microsoft.Spark.UnitTest
+{
+    public class CollectionUtilsTests
+    {
+        [Fact]
+        public void TestArrayEquals()
+        {
+            Assert.False(CollectionUtils.ArrayEquals(new int[] { 1 }, null));
+            Assert.False(CollectionUtils.ArrayEquals(null, new int[] { 1 }));
+            Assert.False(CollectionUtils.ArrayEquals(new int[] { }, new int[] { 1 }));
+            Assert.False(CollectionUtils.ArrayEquals(new int[] { 1 }, new int[] { }));
+            Assert.False(CollectionUtils.ArrayEquals(new int[] { 1 }, new int[] { 1, 2 }));
+            Assert.False(CollectionUtils.ArrayEquals(new int[] { 1 }, new int[] { 2 }));
+
+            Assert.True(CollectionUtils.ArrayEquals<object>(null, null));
+            Assert.True(CollectionUtils.ArrayEquals(new int[] { 1 }, new int[] { 1 }));
+        }
+    }
+}
