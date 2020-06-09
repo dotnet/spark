ed62f3820d1d40f55907c4ad20af176eb74ec43c
John Baro <johnbaro@gmail.com>
2020-06-10 08:32:18 +1000

    Wrap spark exceptions per #472

---
+++ src/csharp/Microsoft.Spark.E2ETest/BridgeTests/JvmBridgeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Spark.Sql;
+using Xunit;
+
+namespace Microsoft.Spark.E2ETest.BridgeTests
+{
+    [Collection("Spark E2E Tests")]
+    public class JvmBridgeTests
+    {
+        [Fact]
+        public void TestSparkExceptionReturned()
+        {
+            using var session = SparkSession.GetDefaultSession();
+            try
+            {
+                session.Sql("THROW!!!");
+            }
+            catch (Exception sparkEx)
+            {
+                Assert.NotNull(sparkEx.InnerException);
+            }
+        }
+    }
+}
---
+++ src/csharp/Microsoft.Spark/JvmException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Spark
+{
+    public class JvmException : Exception
+    {
+        private readonly string _jvmFullException;
+
+        public JvmException(string jvmFullException)
+        {
+            _jvmFullException = jvmFullException;
+        }
+
+        public override string Message => _jvmFullException;
+    }
+}
