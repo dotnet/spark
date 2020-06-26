3106e8e87f811979cd0a5a1957ac6bcf177be2ce
John Baro <johnbaro@gmail.com>
2020-06-27 04:42:29 +1000

    Expose JVM exceptions (#566)
    * Wrap spark exceptions per #472
    
    * Extra tests for JvmException
    Code styling per guidelines from review by @imback82 in #541
    
    * Add JvmBridge doc link
    
    Co-authored-by: Steve Suh <suhsteve@gmail.com>
    
    * Fix per code guidelines
    
    Co-authored-by: Steve Suh <suhsteve@gmail.com>
    
    * Fix cref link
    
    * Formatting
    
    Co-authored-by: Steve Suh <suhsteve@gmail.com>
    
    * Add license header
    
    Co-authored-by: Steve Suh <suhsteve@gmail.com>

---
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/JvmBridgeTests.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.Spark.Sql;
+using Xunit;
+
+namespace Microsoft.Spark.E2ETest.IpcTests
+{
+    [Collection("Spark E2E Tests")]
+    public class JvmBridgeTests
+    {
+        private readonly SparkSession _spark;
+
+        public JvmBridgeTests(SparkFixture fixture)
+        {
+            _spark = fixture.Spark;
+        }
+
+        [Fact]
+        public void TestInnerJvmException()
+        {
+            try
+            {
+                _spark.Sql("THROW!!!");
+            }
+            catch (Exception ex)
+            {
+                Assert.NotNull(ex.InnerException);
+                Assert.IsType<JvmException>(ex.InnerException);
+                Assert.False(string.IsNullOrWhiteSpace(ex.InnerException.Message));
+            }
+        }
+    }
+}
