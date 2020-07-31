c7baf7231914b10300175e67158b604d646b97d4
GOEddieUK <goeddie@gmail.com>
2020-07-31 19:56:29 +0100

    too many lines

--- src/csharp/Microsoft.Spark/ML/Feature/FeatureBase.cs
+++ src/csharp/Microsoft.Spark/ML/Feature/FeatureBase.cs
@@ -106,11 +106,10 @@ namespace Microsoft.Spark.ML.Feature
                 {
                     ParameterInfo[] parameters = c.GetParameters();
                     return (parameters.Length == 1) &&
-                        (parameters[0].ParameterType == typeof(JvmObjectReference));
+                           (parameters[0].ParameterType == typeof(JvmObjectReference));
                 });
 
             return (T)constructor.Invoke(new object[] {reference});
         }
-        
     }
 }
