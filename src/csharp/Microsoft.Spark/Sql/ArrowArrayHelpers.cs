da414e87f67bef887e2d6596aa17388840ad55e3
hiMinju <minju980508@gmail.com>
2020-09-05 14:08:35 +0900

    Move the type var outside the switch and used _ inplace

--- src/csharp/Microsoft.Spark/Sql/ArrowArrayHelpers.cs
+++ src/csharp/Microsoft.Spark/Sql/ArrowArrayHelpers.cs
@@ -41,31 +41,32 @@ namespace Microsoft.Spark.Sql
 
         public static DataFrameColumn CreateEmptyColumn<T>()
         {
-            return typeof(T) switch
+            Type type = typeof(T);
+            return type switch
             {
-                Type t when t == typeof(BooleanDataFrameColumn) =>
+                _ when type == typeof(BooleanDataFrameColumn) =>
                     new BooleanDataFrameColumn("Empty"),
-                Type t when t == typeof(SByteDataFrameColumn) =>
+                _ when type == typeof(SByteDataFrameColumn) =>
                     new SByteDataFrameColumn("Empty"),
-                Type t when t == typeof(ByteDataFrameColumn) =>
+                _ when type == typeof(ByteDataFrameColumn) =>
                     new ByteDataFrameColumn("Empty"),
-                Type t when t == typeof(Int16DataFrameColumn) =>
+                _ when type == typeof(Int16DataFrameColumn) =>
                     new Int16DataFrameColumn("Empty"),
-                Type t when t == typeof(UInt16DataFrameColumn) =>
+                _ when type == typeof(UInt16DataFrameColumn) =>
                     new UInt16DataFrameColumn("Empty"),
-                Type t when t == typeof(Int32DataFrameColumn) =>
+                _ when type == typeof(Int32DataFrameColumn) =>
                     new Int32DataFrameColumn("Empty"),
-                Type t when t == typeof(UInt32DataFrameColumn) =>
+                _ when type == typeof(UInt32DataFrameColumn) =>
                     new UInt32DataFrameColumn("Empty"),
-                Type t when t == typeof(Int64DataFrameColumn) =>
+                _ when type == typeof(Int64DataFrameColumn) =>
                     new Int64DataFrameColumn("Empty"),
-                Type t when t == typeof(UInt64DataFrameColumn) =>
+                _ when type == typeof(UInt64DataFrameColumn) =>
                     new UInt64DataFrameColumn("Empty"),
-                Type t when t == typeof(SingleDataFrameColumn) =>
+                _ when type == typeof(SingleDataFrameColumn) =>
                     new SingleDataFrameColumn("Empty"),
-                Type t when t == typeof(DoubleDataFrameColumn) =>
+                _ when type == typeof(DoubleDataFrameColumn) =>
                     new DoubleDataFrameColumn("Empty"),
-                Type t when t == typeof(ArrowStringDataFrameColumn) =>
+                _ when type == typeof(ArrowStringDataFrameColumn) =>
                     new ArrowStringDataFrameColumn("Empty"),
 
                 _ => throw new NotSupportedException($"Unknown type: {typeof(T)}")
@@ -86,37 +87,38 @@ namespace Microsoft.Spark.Sql
 
         private static ArrayData BuildEmptyArrayDataFromArrayType<T>()
         {
-            return typeof(T) switch
+            Type type = typeof(T);
+            return type switch
             {
-                Type t when t == typeof(BooleanArray) =>
+                _ when type == typeof(BooleanArray) =>
                     BuildEmptyArrayDataFromArrowType(BooleanType.Default),
-                Type t when t == typeof(Int8Array) =>
+                _ when type == typeof(Int8Array) =>
                     BuildEmptyArrayDataFromArrowType(Int8Type.Default),
-                Type t when t == typeof(UInt8Array) =>
+                _ when type == typeof(UInt8Array) =>
                     BuildEmptyArrayDataFromArrowType(UInt8Type.Default),
-                Type t when t == typeof(Int16Array) =>
+                _ when type == typeof(Int16Array) =>
                     BuildEmptyArrayDataFromArrowType(Int16Type.Default),
-                Type t when t == typeof(UInt16Array) =>
+                _ when type == typeof(UInt16Array) =>
                     BuildEmptyArrayDataFromArrowType(UInt16Type.Default),
-                Type t when t == typeof(Int32Array) =>
+                _ when type == typeof(Int32Array) =>
                     BuildEmptyArrayDataFromArrowType(Int32Type.Default),
-                Type t when t == typeof(UInt32Array) =>
+                _ when type == typeof(UInt32Array) =>
                     BuildEmptyArrayDataFromArrowType(UInt32Type.Default),
-                Type t when t == typeof(Int64Array) =>
+                _ when type == typeof(Int64Array) =>
                     BuildEmptyArrayDataFromArrowType(Int64Type.Default),
-                Type t when t == typeof(UInt64Array) =>
+                _ when type == typeof(UInt64Array) =>
                     BuildEmptyArrayDataFromArrowType(UInt64Type.Default),
-                Type t when t == typeof(FloatArray) =>
+                _ when type == typeof(FloatArray) =>
                     BuildEmptyArrayDataFromArrowType(FloatType.Default),
-                Type t when t == typeof(DoubleArray) =>
+                _ when type == typeof(DoubleArray) =>
                     BuildEmptyArrayDataFromArrowType(DoubleType.Default),
-                Type t when t == typeof(Date64Array) =>
+                _ when type == typeof(Date64Array) =>
                     BuildEmptyArrayDataFromArrowType(Date64Type.Default),
-                Type t when t == typeof(TimestampArray) =>
+                _ when type == typeof(TimestampArray) =>
                     BuildEmptyArrayDataFromArrowType(TimestampType.Default),
-                Type t when t == typeof(StringArray) =>
+                _ when type == typeof(StringArray) =>
                     BuildEmptyArrayDataFromArrowType(StringType.Default),
-                Type t when t == typeof(BinaryArray) =>
+                _ when type == typeof(BinaryArray) =>
                     BuildEmptyArrayDataFromArrowType(BinaryType.Default),
 
                 _ => throw new NotSupportedException($"Unknown type: {typeof(T)}")
