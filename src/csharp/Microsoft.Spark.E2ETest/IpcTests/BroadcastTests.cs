b7aaa542b2a0cd2538935da9d5029390d795a8d1
Niharika Dutta <nidutta@microsoft.com>
2020-09-17 17:11:40 -0700

    removing encryption false from TestBroadcastLargeFalse

--- src/csharp/Microsoft.Spark.E2ETest/IpcTests/BroadcastTests.cs
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/BroadcastTests.cs
@@ -60,7 +60,6 @@ namespace Microsoft.Spark.E2ETest.IpcTests
         /// </summary>
         [Theory]
         [InlineData("true")]
-        [InlineData("false")]
         public void TestLargeBroadcastValue(string isEncryptionEnabled)
         {
             _spark.SparkContext.GetConf().Set("spark.io.encryption.enabled", isEncryptionEnabled);
