184441f20867307633f547eceb7cec1e658b81f0
Andrew Fogarty <anfog@microsoft.com>
2020-10-01 17:14:46 -0700

    Hyperspace version annotations

---
+++ src/csharp/Extensions/Microsoft.Spark.Extensions.Hyperspace/HyperspaceVersions.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.Spark.Extensions.Hyperspace
+{
+    internal static class HyperspaceVersions
+    {
+        internal const string V0_0_1 = "0.0.1";
+        internal const string V0_0_2 = "0.0.2";
+    }
+}
