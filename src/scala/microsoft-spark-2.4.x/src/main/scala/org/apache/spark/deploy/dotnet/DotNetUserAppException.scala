52376add3a5076ea7dac50dde5ea08f2a03fb315
Andrew Fogarty <anfog@microsoft.com>
2020-10-09 17:12:26 -0700

    Don't need val

--- src/scala/microsoft-spark-2.4.x/src/main/scala/org/apache/spark/deploy/dotnet/DotNetUserAppException.scala
+++ src/scala/microsoft-spark-2.4.x/src/main/scala/org/apache/spark/deploy/dotnet/DotNetUserAppException.scala
@@ -8,7 +8,7 @@ import org.apache.spark.SparkException
  * @param exitCode Exit code returned by the .NET application.
  * @param dotNetStackTrace Stacktrace extracted from .NET application logs.
  */
-private[spark] class DotNetUserAppException(val exitCode: Int, dotNetStackTrace: Option[String])
+private[spark] class DotNetUserAppException(exitCode: Int, dotNetStackTrace: Option[String])
     extends SparkException(dotNetStackTrace match {
       case None => s"User application exited with $exitCode"
       case Some(e) => s"User application exited with $exitCode and .NET exception: $e"
