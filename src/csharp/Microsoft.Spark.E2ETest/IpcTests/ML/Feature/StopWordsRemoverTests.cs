fb203d7f0daffaf1e074de67b5a47765d6b2af02
Saravana Kumar <saravananbscitm@gmail.com>
2020-10-10 13:42:17 +0530

    Git remove lines to debug

--- src/csharp/Microsoft.Spark.E2ETest/IpcTests/ML/Feature/StopWordsRemoverTests.cs
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/ML/Feature/StopWordsRemoverTests.cs
@@ -23,31 +23,31 @@ namespace Microsoft.Spark.E2ETest.IpcTests.ML.Feature
             string expectedInputCol = "input_col";
             string expectedOutputCol = "output_col";
 
-            var input = _spark.Sql("SELECT 'hello I AM a string TO, StopWordsRemover' as input_col" +
-                                   " from range(100)");
+            // var input = _spark.Sql("SELECT 'hello I AM a string TO, StopWordsRemover' as input_col" +
+            //                        " from range(100)");
 
             var stopWordsRemover = new StopWordsRemover(expectedUid)
                 .SetInputCol(expectedInputCol)
                 .SetOutputCol(expectedOutputCol);
 
-            var output = stopWordsRemover.Transform(input);
+            // var output = stopWordsRemover.Transform(input);
 
-            Assert.Contains(output.Schema().Fields, (f => f.Name == expectedOutputCol));
+            // Assert.Contains(output.Schema().Fields, (f => f.Name == expectedOutputCol));
             Assert.Equal(expectedInputCol, stopWordsRemover.GetInputCol());
             Assert.Equal(expectedOutputCol, stopWordsRemover.GetOutputCol());
 
-            using (var tempDirectory = new TemporaryDirectory())
-            {
-                string savePath = Path.Join(tempDirectory.Path, "StopWordsRemover");
-                stopWordsRemover.Save(savePath);
-
-                var loadedStopWordsRemover = Tokenizer.Load(savePath);
-                Assert.Equal(stopWordsRemover.Uid(), loadedStopWordsRemover.Uid());
-            }
-
-            Assert.Equal(expectedUid, stopWordsRemover.Uid());
-
-            TestFeatureBase(stopWordsRemover, "inputCol", "input_col");
+            // using (var tempDirectory = new TemporaryDirectory())
+            // {
+            //     string savePath = Path.Join(tempDirectory.Path, "StopWordsRemover");
+            //     stopWordsRemover.Save(savePath);
+            //
+            //     var loadedStopWordsRemover = Tokenizer.Load(savePath);
+            //     Assert.Equal(stopWordsRemover.Uid(), loadedStopWordsRemover.Uid());
+            // }
+            //
+            // Assert.Equal(expectedUid, stopWordsRemover.Uid());
+            //
+            // TestFeatureBase(stopWordsRemover, "inputCol", "input_col");
         }
     }
 }
