7c25fcb5e6de4e33636febbb4c27554ac64009de
Andrew Fogarty <anfog@microsoft.com>
2020-12-01 16:11:08 -0800

    Basic FileSystem implementation

---
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/FileSystemTests.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Microsoft.Spark.Sql;
+using Microsoft.Spark.UnitTest.TestUtils;
+using Microsoft.Spark.Utils;
+using Xunit;
+
+namespace Microsoft.Spark.E2ETest.IpcTests
+{
+    [Collection("Spark E2E Tests")]
+    public class FileSystemTests
+    {
+        private readonly SparkSession _spark;
+
+        public FileSystemTests(SparkFixture fixture)
+        {
+            _spark = fixture.Spark;
+        }
+
+        /// <summary>
+        /// Test that methods return the expected signature.
+        /// </summary>
+        [Fact]
+        public void TestSignatures()
+        {
+            using IFileSystem fs = FileSystem.Get(_spark.SparkContext);
+
+            using var tempDirectory = new TemporaryDirectory();
+            string path = Path.Combine(tempDirectory.Path, "temp-table");
+            _spark.Range(25).Write().Format("parquet").Save(path);
+
+            Assert.True(fs.Delete(path, true));
+            Assert.False(fs.Delete(path, true));
+        }
+    }
+}
