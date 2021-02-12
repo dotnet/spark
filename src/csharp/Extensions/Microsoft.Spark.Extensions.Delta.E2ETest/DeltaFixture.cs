fe03c7431e174811fa51c84055343bc6e6025b80
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2021-02-12 11:32:09 -0800

    Support for Delta Lake 0.8.0 (#823)

--- src/csharp/Extensions/Microsoft.Spark.Extensions.Delta.E2ETest/DeltaFixture.cs
+++ src/csharp/Extensions/Microsoft.Spark.Extensions.Delta.E2ETest/DeltaFixture.cs
@@ -19,14 +19,16 @@ namespace Microsoft.Spark.Extensions.Delta.E2ETest
             string deltaVersion = sparkVersion.Major switch
             {
                 2 => "delta-core_2.11:0.6.1",
-                3 => "delta-core_2.12:0.7.0",
+                3 => "delta-core_2.12:0.8.0",
                 _ => throw new NotSupportedException($"Spark {sparkVersion} not supported.")
             };
 
             (string, string)[] conf = new[]
             {
                 ("spark.databricks.delta.snapshotPartitions", "2"),
-                ("spark.sql.sources.parallelPartitionDiscovery.parallelism", "5")
+                ("spark.sql.sources.parallelPartitionDiscovery.parallelism", "5"),
+                // Set the writer protocol version for testing UpgradeTableProtocol().
+                ("spark.databricks.delta.minWriterVersion", "2")
             };
 
             (string, string)[] extraConf = sparkVersion.Major switch
