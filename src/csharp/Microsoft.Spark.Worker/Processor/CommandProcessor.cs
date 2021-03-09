404253853586df72426eeadd2f5b198983f2ee8d
Steve Suh <suhsteve@gmail.com>
2021-03-08 23:41:44 -0800

    initial commit

--- src/csharp/Microsoft.Spark.Worker/Processor/CommandProcessor.cs
+++ src/csharp/Microsoft.Spark.Worker/Processor/CommandProcessor.cs
@@ -100,6 +100,7 @@ namespace Microsoft.Spark.Worker.Processor
                 (2, 3) => SqlCommandProcessorV2_3_X.Process(evalType, stream),
                 (2, 4) => SqlCommandProcessorV2_4_X.Process(evalType, stream),
                 (3, 0) => SqlCommandProcessorV2_4_X.Process(evalType, stream),
+                (3, 1) => SqlCommandProcessorV2_4_X.Process(evalType, stream),
                 _ => throw new NotSupportedException($"Spark {version} not supported.")
             };
         }
