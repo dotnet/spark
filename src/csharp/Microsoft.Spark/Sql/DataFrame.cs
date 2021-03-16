ca930cf4a073158e08793ebf890491c462496ab8
Steve Suh <suhsteve@gmail.com>
2021-03-15 20:24:36 -0700

    adjust pattern match

--- src/csharp/Microsoft.Spark/Sql/DataFrame.cs
+++ src/csharp/Microsoft.Spark/Sql/DataFrame.cs
@@ -1057,8 +1057,7 @@ namespace Microsoft.Spark.Sql
                 // string to use for the authentication.
                 (2, 3, _) => ParseConnectionInfo(result, false),
                 (2, 4, _) => ParseConnectionInfo(result, false),
-                (3, 0, _) => ParseConnectionInfo(result, false),
-                (3, 1, _) => ParseConnectionInfo(result, false),
+                (3, _, _) => ParseConnectionInfo(result, false),
                 _ => throw new NotSupportedException($"Spark {version} not supported.")
             };
         }
