aac0ca7ea2a601e0da5c1d63a44144015a1525a3
Steve Suh <suhsteve@gmail.com>
2021-03-22 21:12:21 -0700

    check exception message

--- src/scala/microsoft-spark-2-4/src/test/scala/org/apache/spark/util/dotnet/UtilsTest.scala
+++ src/scala/microsoft-spark-2-4/src/test/scala/org/apache/spark/util/dotnet/UtilsTest.scala
@@ -8,7 +8,7 @@ package org.apache.spark.util.dotnet
 
 import org.apache.spark.SparkConf
 import org.apache.spark.internal.config.dotnet.Dotnet.DOTNET_IGNORE_SPARK_PATCH_VERSION_CHECK
-import org.junit.Assert.assertThrows
+import org.junit.Assert.{assertEquals, assertThrows}
 import org.junit.Test
 import org.junit.function.ThrowingRunnable
 
@@ -39,22 +39,29 @@ class UtilsTest {
     conf.set(DOTNET_IGNORE_SPARK_PATCH_VERSION_CHECK, false)
 
     val sparkVersion = "2.4.8"
+    val normalizedSparkVersion = Utils.normalizeSparkVersion(sparkVersion)
     val sparkMajorMinorVersionPrefix = "2.4."
     val supportedSparkVersions =
       Set[String]("2.4.0", "2.4.1", "2.4.3", "2.4.4", "2.4.5", "2.4.6", "2.4.7")
 
-    assertThrows(
+    val exception = assertThrows(
       classOf[IllegalArgumentException],
       new ThrowingRunnable {
         override def run(): Unit = {
           Utils.validateSparkVersions(
             conf,
             sparkVersion,
-            Utils.normalizeSparkVersion(sparkVersion),
+            normalizedSparkVersion,
             sparkMajorMinorVersionPrefix,
             supportedSparkVersions)
         }
       })
+
+    assertEquals(
+      s"Unsupported spark version used: $sparkVersion. " +
+        s"Normalized spark version used: $normalizedSparkVersion. " +
+        s"Supported versions: ${supportedSparkVersions.toSeq.sorted.mkString(", ")}",
+      exception.getMessage)
   }
 
   @Test
@@ -63,21 +70,28 @@ class UtilsTest {
     conf.set(DOTNET_IGNORE_SPARK_PATCH_VERSION_CHECK, false)
 
     val sparkVersion = "3.0.2"
+    val normalizedSparkVersion = Utils.normalizeSparkVersion(sparkVersion)
     val sparkMajorMinorVersionPrefix = "2.4."
     val supportedSparkVersions =
       Set[String]("2.4.0", "2.4.1", "2.4.3", "2.4.4", "2.4.5", "2.4.6", "2.4.7")
 
-    assertThrows(
+    val exception = assertThrows(
       classOf[IllegalArgumentException],
       new ThrowingRunnable {
         override def run(): Unit = {
           Utils.validateSparkVersions(
             conf,
             sparkVersion,
-            Utils.normalizeSparkVersion(sparkVersion),
+            normalizedSparkVersion,
             sparkMajorMinorVersionPrefix,
             supportedSparkVersions)
         }
       })
+
+    assertEquals(
+      s"Unsupported spark version used: $sparkVersion. " +
+        s"Normalized spark version used: $normalizedSparkVersion. " +
+        s"Supported spark major.minor version: $sparkMajorMinorVersionPrefix",
+      exception.getMessage)
   }
 }
--- src/scala/microsoft-spark-3-0/src/test/scala/org/apache/spark/util/dotnet/UtilsTest.scala
+++ src/scala/microsoft-spark-3-0/src/test/scala/org/apache/spark/util/dotnet/UtilsTest.scala
@@ -8,7 +8,7 @@ package org.apache.spark.util.dotnet
 
 import org.apache.spark.SparkConf
 import org.apache.spark.internal.config.dotnet.Dotnet.DOTNET_IGNORE_SPARK_PATCH_VERSION_CHECK
-import org.junit.Assert.assertThrows
+import org.junit.Assert.{assertEquals, assertThrows}
 import org.junit.Test
 
 @Test
@@ -37,19 +37,26 @@ class UtilsTest {
     conf.set(DOTNET_IGNORE_SPARK_PATCH_VERSION_CHECK, false)
 
     val sparkVersion = "3.0.3"
+    val normalizedSparkVersion = Utils.normalizeSparkVersion(sparkVersion)
     val sparkMajorMinorVersionPrefix = "3.0."
     val supportedSparkVersions = Set[String]("3.0.0", "3.0.1", "3.0.2")
 
-    assertThrows(
+    val exception = assertThrows(
       classOf[IllegalArgumentException],
       () => {
         Utils.validateSparkVersions(
           conf,
           sparkVersion,
-          Utils.normalizeSparkVersion(sparkVersion),
+          normalizedSparkVersion,
           sparkMajorMinorVersionPrefix,
           supportedSparkVersions)
       })
+
+    assertEquals(
+      s"Unsupported spark version used: $sparkVersion. " +
+        s"Normalized spark version used: $normalizedSparkVersion. " +
+        s"Supported versions: ${supportedSparkVersions.toSeq.sorted.mkString(", ")}",
+      exception.getMessage)
   }
 
   @Test
@@ -58,18 +65,25 @@ class UtilsTest {
     conf.set(DOTNET_IGNORE_SPARK_PATCH_VERSION_CHECK, false)
 
     val sparkVersion = "2.4.4"
+    val normalizedSparkVersion = Utils.normalizeSparkVersion(sparkVersion)
     val sparkMajorMinorVersionPrefix = "3.0."
     val supportedSparkVersions = Set[String]("3.0.0", "3.0.1", "3.0.2")
 
-    assertThrows(
+    val exception = assertThrows(
       classOf[IllegalArgumentException],
       () => {
         Utils.validateSparkVersions(
           conf,
           sparkVersion,
-          Utils.normalizeSparkVersion(sparkVersion),
+          normalizedSparkVersion,
           sparkMajorMinorVersionPrefix,
           supportedSparkVersions)
       })
+
+    assertEquals(
+      s"Unsupported spark version used: $sparkVersion. " +
+        s"Normalized spark version used: $normalizedSparkVersion. " +
+        s"Supported spark major.minor version: $sparkMajorMinorVersionPrefix",
+      exception.getMessage)
   }
 }
