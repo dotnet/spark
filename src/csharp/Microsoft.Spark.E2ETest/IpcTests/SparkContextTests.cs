4841a6776443acc4e92ab1e54b07f77bbc7ed296
Steve Suh <suhsteve@gmail.com>
2021-04-08 16:06:00 -0700

    update test

--- src/csharp/Microsoft.Spark.E2ETest/IpcTests/SparkContextTests.cs
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/SparkContextTests.cs
@@ -47,9 +47,9 @@ namespace Microsoft.Spark.E2ETest.IpcTests
 
             Assert.IsType<string[]>(sc.ListFiles().ToArray());
 
-            Assert.Null(sc.GetCheckpointDir());
             using var tempDir = new TemporaryDirectory();
             sc.SetCheckpointDir(TestEnvironment.ResourceDirectory);
+
             Assert.IsType<string>(sc.GetCheckpointDir());
 
             Assert.IsType<Configuration>(sc.HadoopConfiguration());
