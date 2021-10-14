4c88ece7f3d86b2e5bdae00ca1bbc936bacc615c
GOEddieUK <goeddie@gmail.com>
2021-10-14 22:39:02 +0100

    test

---
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/Sql/DataTypesTests.cs
@@ -0,0 +1,64 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Spark.Sql;
+using Microsoft.Spark.Sql.Types;
+using Xunit;
+
+namespace Microsoft.Spark.E2ETest.IpcTests
+{
+
+    [Collection("Spark E2E Tests")]    
+    public class DataTypesTests
+    {
+        private readonly SparkSession _spark;
+
+        public DataTypesTests(SparkFixture fixture)
+        {
+            _spark = fixture.Spark;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void TestDecimalType()
+        {
+            var df = _spark.CreateDataFrame(
+                new List<GenericRow>
+                {
+                    new GenericRow(
+                        new object[]
+                        {
+                            decimal.MinValue, decimal.MaxValue, decimal.Zero, decimal.MinusOne,
+                            new object[]
+                            {
+                                decimal.MinValue, decimal.MaxValue, decimal.Zero, decimal.MinusOne
+                            }
+                        }),
+                },
+                new StructType(
+                    new List<StructField>()
+                    {
+                        new StructField("min", new DecimalType(38, 0)),
+                        new StructField("max", new DecimalType(38, 0)),
+                        new StructField("zero", new DecimalType(38, 0)),
+                        new StructField("minusOne", new DecimalType(38, 0)),
+                        new StructField("array", new ArrayType(new DecimalType(38,0)))
+                    }));
+
+            Row row = df.Collect().First();
+            Assert.Equal(decimal.MinValue, row[0]);
+            Assert.Equal(decimal.MaxValue, row[1]);
+            Assert.Equal(decimal.Zero, row[2]);
+            Assert.Equal(decimal.MinusOne, row[3]);
+            Assert.Equal(new object[]{decimal.MinValue, decimal.MaxValue, decimal.Zero, decimal.MinusOne}, 
+                row[4]);
+        }
+
+    }
+}
