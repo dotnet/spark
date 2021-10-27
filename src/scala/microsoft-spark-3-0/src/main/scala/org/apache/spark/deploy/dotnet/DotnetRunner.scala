89571c17abe31fce95c04de8a44356563043d204
Steve Suh <suhsteve@gmail.com>
2021-10-26 22:14:02 -0700

    remove 3.0.2

--- src/scala/microsoft-spark-3-0/src/main/scala/org/apache/spark/deploy/dotnet/DotnetRunner.scala
+++ src/scala/microsoft-spark-3-0/src/main/scala/org/apache/spark/deploy/dotnet/DotnetRunner.scala
@@ -36,7 +36,7 @@ import scala.util.Try
 object DotnetRunner extends Logging {
   private val DEBUG_PORT = 5567
   private val supportedSparkMajorMinorVersionPrefix = "3.0"
-  private val supportedSparkVersions = Set[String]("3.0.0", "3.0.1", "3.0.2")
+  private val supportedSparkVersions = Set[String]("3.0.0", "3.0.1")
 
   val SPARK_VERSION = DotnetUtils.normalizeSparkVersion(spark.SPARK_VERSION)
 
