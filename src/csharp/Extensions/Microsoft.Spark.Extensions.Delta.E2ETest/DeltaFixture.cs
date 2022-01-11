c2aa30a665a6ec53a42107b02e5264f56c42c20e
Niharika Dutta <nidutta@microsoft.com>
2022-01-11 10:08:43 -0800

    Fix delta tests

--- src/csharp/Extensions/Microsoft.Spark.Extensions.Delta.E2ETest/DeltaFixture.cs
+++ src/csharp/Extensions/Microsoft.Spark.Extensions.Delta.E2ETest/DeltaFixture.cs
@@ -19,6 +19,7 @@ namespace Microsoft.Spark.Extensions.Delta.E2ETest
             string deltaVersion = (sparkVersion.Major, sparkVersion.Minor) switch
             {
                 (2 , _)=> "delta-core_2.11:0.6.1",
+                (3, 0) => "delta-core_2.12:0.8.0",
                 (3, 1) => "delta-core_2.12:0.8.0",
                 (3, 2) => "delta-core_2.12:1.1.0",
                 _ => throw new NotSupportedException($"Spark {sparkVersion} not supported.")
