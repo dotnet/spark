61b1d2128dd6d4aa62c409da3185ea402cd36d9f
Niharika Dutta <nidutta@microsoft.com>
2022-01-14 13:32:19 -0800

    Add simple test suite

---
+++ src/csharp/TestProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.Write("Hello world 1!");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Console.Write("Hello world 2!");
+        }
+    }
+   
+}
