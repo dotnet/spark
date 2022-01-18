b06f3a9157816405aa877771c66db5953525f4a9
Niharika Dutta <nidutta@microsoft.com>
2022-01-18 15:03:20 -0800

    add simple tests

---
+++ src/csharp/TestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("hello world 1");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Console.WriteLine("hello world 2");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Console.WriteLine("hello world 3");
+        }
+    }
+}
