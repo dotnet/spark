b79e4cc2e1ab7fcd3338a243190402c89ef0d4cd
Niharika Dutta <nidutta@microsoft.com>
2022-01-19 16:25:58 -0800

    Run Delta tests

--- src/csharp/Extensions/Microsoft.Spark.Extensions.Delta.E2ETest/DeltaFixture.cs
+++ src/csharp/Extensions/Microsoft.Spark.Extensions.Delta.E2ETest/DeltaFixture.cs
@@ -16,10 +16,12 @@ namespace Microsoft.Spark.Extensions.Delta.E2ETest
         public DeltaFixture()
         {
             Version sparkVersion = SparkSettings.Version;
-            string deltaVersion = sparkVersion.Major switch
+            string deltaVersion = (sparkVersion.Major, sparkVersion.Minor) switch
             {
-                2 => "delta-core_2.11:0.6.1",
-                3 => "delta-core_2.12:0.8.0",
+                (2, _) => "delta-core_2.11:0.6.1",
+                (3, 0) => "delta-core_2.12:0.8.0",
+                (3, 1) => "delta-core_2.12:0.8.0",
+                (3, 2) => "delta-core_2.12:1.1.0",
                 _ => throw new NotSupportedException($"Spark {sparkVersion} not supported.")
             };
 
