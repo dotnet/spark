0d6fd60a8002751b72789e05816e92bea031ec01
Niharika Dutta <nidutta@microsoft.com>
2022-04-20 13:27:38 -0700

    update repo for Spark download

--- src/csharp/Microsoft.Spark.E2ETest/SparkFixture.cs
+++ src/csharp/Microsoft.Spark.E2ETest/SparkFixture.cs
@@ -162,6 +162,12 @@ namespace Microsoft.Spark.E2ETest
                 Path.Combine(_tempDirectory.Path, "spark-warehouse")).AbsoluteUri;
             string warehouseDir = $"--conf spark.sql.warehouse.dir={warehouseUri}";
 
+            // Spark24 < 2.4.8 and Spark30 < 3.0.3 use bintray as the repository
+            // service for spark-packages. As of  May 1st, 2021 bintray has been sunset and is no
+            // longer available. Specify additional remote repositories to search for the maven
+            // coordinates given with --packages.
+            string repositories = "--repositories https://repos.spark-packages.org/";
+
             string extraArgs = Environment.GetEnvironmentVariable(
                 EnvironmentVariableNames.ExtraSparkSubmitArgs) ?? "";
 
@@ -175,7 +181,8 @@ namespace Microsoft.Spark.E2ETest
             string logOption = "--conf spark.driver.extraJavaOptions=-Dlog4j.configuration=" +
                 $"{resourceUri}/log4j.properties";
 
-            args = $"{logOption} {warehouseDir} {extraArgs} {classArg} --master local {jar} debug";
+            args = $"{logOption} {warehouseDir} {extraArgs} {repositories} {classArg} " +
+                $"--master local {jar} debug";
         }
 
         private string GetJarPrefix()
