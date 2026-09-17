e722c5e5fab15122c174fde3aa764f52f7f2b206
Shinai Yang <shinyang@microsoft.com>
2026-09-17 11:42:39 +0800

    Fix ML Param construction on Spark 4.0.0

--- src/csharp/Microsoft.Spark.E2ETest/IpcTests/ML/Param/ParamTests.cs
+++ src/csharp/Microsoft.Spark.E2ETest/IpcTests/ML/Param/ParamTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Spark.ML.Feature;
 using Microsoft.Spark.ML.Feature.Param;
 using Microsoft.Spark.Sql;
 using Xunit;
@@ -19,14 +20,18 @@ namespace Microsoft.Spark.E2ETest.IpcTests.ML.ParamTests
             _spark = fixture.Spark;
         }
 
-        [Fact]
-        public void Test()
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Test(bool useIdentifiableParent)
         {
             const string expectedParent = "parent";
             const string expectedName = "name";
             const string expectedDoc = "doc";
 
-            var param = new Param(expectedParent, expectedName, expectedDoc);
+            var param = useIdentifiableParent
+                ? new Param(new Tokenizer(expectedParent), expectedName, expectedDoc)
+                : new Param(expectedParent, expectedName, expectedDoc);
 
             Assert.Equal(expectedParent, param.Parent);
             Assert.Equal(expectedDoc, param.Doc);
